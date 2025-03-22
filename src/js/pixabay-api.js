0058fae5ef78a384d027426c693dc0059f4b04c8
Savranin-Denis <savranindenisv@gmail.com>
2025-03-22 14:42:08 +0200

    task gallery test ver 1.0 done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,31 +1,6 @@
-// import axios from 'axios';
-
-// const API_KEY = '49352946-bc37130ddbdb199550e5d4773';
-// const url = 'https://pixabay.com/api/';
-
-// export async function searchImages(query, page = 1, perPage = 15) {
-//   try {
-//     const response = await axios.get(url, {
-//       params: {
-//         key: API_KEY,
-//         q: query,
-//         image_type: 'photo',
-//         orientation: 'horizontal',
-//         safesearch: true,
-//         per_page: perPage,
-//         page: page,
-//       },
-//     });
-//     return response.data.hits;
-//   } catch (error) {
-//     console.error('Error fetching images:', error);
-//     throw error;
-//   }
-// }
-
 import axios from 'axios';
 
-const API_KEY = '49352946-bc37130ddbdb199550e5d4773';
+const API_KEY = import.meta.env.VITE_GALLERY_KEY;
 const url = 'https://pixabay.com/api/';
 
 export async function searchImages(query, page = 1, perPage = 15) {
--- src/main.js
+++ src/main.js
@@ -1,82 +1,3 @@
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
-// import { searchImages } from './js/pixabay-api';
-// import { displayImages } from './js/render-functions';
-
-// const form = document.querySelector('.form');
-// const gallery = document.querySelector('.gallery');
-// const loader = document.querySelector('.loader');
-// const loadMoreBtn = document.querySelector('.loader-btn');
-
-// let searchText = '';
-// let page = 1;
-// const perPage = 15;
-
-// loader.style.display = 'none';
-// loadMoreBtn.style.display = 'none';
-
-// form.addEventListener('submit', async event => {
-//   event.preventDefault();
-//   const newSearchText = form.elements['search-text'].value.trim();
-
-//   if (!newSearchText) {
-//     iziToast.info({
-//       title: 'Info',
-//       position: 'topRight',
-//       message: 'Enter a search term!',
-//     });
-//     return;
-//   }
-
-//   if (newSearchText !== searchText) {
-//     searchText = newSearchText;
-//     page = 1;
-//     gallery.innerHTML = '';
-//     loadMoreBtn.style.display = 'none';
-//   }
-
-//   loader.style.display = 'block';
-
-//   try {
-//     const images = await searchImages(searchText, page, perPage);
-//     displayImages(images, gallery, page > 1);
-//     if (images.length === perPage) {
-//       loadMoreBtn.style.display = 'block';
-//     } else {
-//       loadMoreBtn.style.display = 'none';
-//     }
-//   } catch (error) {
-//     iziToast.error({
-//       title: 'Error',
-//       position: 'topRight',
-//       message: 'There was an error loading the images. Please try again later.',
-//     });
-//   } finally {
-//     loader.style.display = 'none';
-//   }
-// });
-
-// loadMoreBtn.addEventListener('click', async () => {
-//   page += 1;
-//   loader.style.display = 'block';
-
-//   try {
-//     const images = await searchImages(searchText, page, perPage);
-//     displayImages(images, gallery, true);
-//     if (images.length < perPage) {
-//       loadMoreBtn.style.display = 'none';
-//     }
-//   } catch (error) {
-//     iziToast.error({
-//       title: 'Error',
-//       position: 'topRight',
-//       message:
-//         'There was an error loading more images. Please try again later.',
-//     });
-//   } finally {
-//     loader.style.display = 'none';
-//   }
-// });
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import { searchImages } from './js/pixabay-api';
@@ -90,10 +11,21 @@ const loadMoreBtn = document.querySelector('.loader-btn');
 let searchText = '';
 let page = 1;
 const perPage = 15;
-let totalHits = 0; // Добавляем переменную для totalHits
+let totalHits = 0;
 
 loader.style.display = 'none';
 loadMoreBtn.style.display = 'none';
+
+function smoothScroll() {
+  const galleryItem = document.querySelector('.gallery-item');
+  if (galleryItem) {
+    const { height } = galleryItem.getBoundingClientRect();
+    window.scrollBy({
+      top: height * 2,
+      behavior: 'smooth',
+    });
+  }
+}
 
 form.addEventListener('submit', async event => {
   event.preventDefault();
@@ -113,7 +45,7 @@ form.addEventListener('submit', async event => {
     page = 1;
     gallery.innerHTML = '';
     loadMoreBtn.style.display = 'none';
-    totalHits = 0; // Сбрасываем totalHits при новом поиске
+    totalHits = 0;
   }
 
   loader.style.display = 'block';
@@ -121,14 +53,14 @@ form.addEventListener('submit', async event => {
   try {
     const data = await searchImages(searchText, page, perPage);
     const images = data.hits;
-    totalHits = data.totalHits; // Получаем totalHits
+    totalHits = data.totalHits;
     displayImages(images, gallery, page > 1);
     if (images.length === perPage && page * perPage < totalHits) {
       loadMoreBtn.style.display = 'block';
-      console.log(totalHits);
     } else {
       loadMoreBtn.style.display = 'none';
     }
+    smoothScroll();
   } catch (error) {
     iziToast.error({
       title: 'Error',
@@ -147,7 +79,7 @@ loadMoreBtn.addEventListener('click', async () => {
   try {
     const data = await searchImages(searchText, page, perPage);
     const images = data.hits;
-    totalHits = data.totalHits; // Получаем totalHits
+    totalHits = data.totalHits;
 
     displayImages(images, gallery, true);
     if (page * perPage >= totalHits) {
@@ -158,6 +90,7 @@ loadMoreBtn.addEventListener('click', async () => {
         message: "We're sorry, but you've reached the end of search results.",
       });
     }
+    smoothScroll();
   } catch (error) {
     iziToast.error({
       title: 'Error',
